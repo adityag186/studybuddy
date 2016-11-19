30f454de393bd43097aa4261805e3f18538cba8a
Vishnu Tadimeti <vishnutadimeti@purdue.edu>
2016-11-19 17:57:56 -0600

    initial commit

---
+++ login/login.controller.js
@@ -0,0 +1,33 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('LoginController', LoginController);
+
+    LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService'];
+    function LoginController($location, AuthenticationService, FlashService) {
+        var me = this;
+
+        me.login = login;
+
+        (function initController() {
+            // reset login status
+            AuthenticationService.ClearCredentials();
+        })();
+
+        function login() {
+            me.dataLoading = true;
+            AuthenticationService.Login(me.username, me.password, function (response) {
+                if (response.success) {
+                    AuthenticationService.SetCredentials(me.username, me.password);
+                    $location.path('/');
+                } else {
+                    FlashService.Error(response.message);
+                    me.dataLoading = false;
+                }
+            });
+        }
+    }
+
+})();
