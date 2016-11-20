96e054153a89165fff0bf0ef7d0053612e34cdf5
Vishnu Tadimeti <vishnutadimeti@purdue.edu>
2016-11-19 18:25:05 -0600

    clean

--- login/login.controller.js
+++ login/login.controller.js
@@ -1,24 +1,17 @@
-﻿(function () {
+﻿(function() {
     'use strict';
-
-    angular
-        .module('app')
-        .controller('LoginController', LoginController);
-
+    angular.module('app').controller('LoginController', LoginController);
     LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService'];
     function LoginController($location, AuthenticationService, FlashService) {
         var me = this;
-
         me.login = login;
-
         (function initController() {
             // reset login status
             AuthenticationService.ClearCredentials();
         })();
-
         function login() {
             me.dataLoading = true;
-            AuthenticationService.Login(me.username, me.password, function (response) {
+            AuthenticationService.Login(me.username, me.password, function(response) {
                 if (response.success) {
                     AuthenticationService.SetCredentials(me.username, me.password);
                     $location.path('/');
@@ -29,5 +22,4 @@
             });
         }
     }
-
 })();
